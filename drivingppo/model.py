092fb381109cfb6331aae8fe719dc8bd5a7ae859
Evergood <arinmandri@gmail.com>
2026-01-28 20:34:05 +0900

    모델 조정 conv 6-6-2

--- drivingppo/model.py
+++ drivingppo/model.py
@@ -61,11 +61,11 @@ class MyFeatureExtractor(BaseFeaturesExtractor):
         self.layer1 = nn.Sequential(
             nn.Conv1d(in_channels=1, out_channels=4, kernel_size=2, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv1d(in_channels=4, out_channels=4, kernel_size=5, stride=2, padding=0),
+            nn.Conv1d(in_channels=4, out_channels=6, kernel_size=6, stride=2, padding=0),
             nn.ReLU(),
             nn.AdaptiveMaxPool1d(output_size=feature1_dim),
             nn.Flatten(),
-            nn.Linear(4 * feature1_dim, feature1_dim)
+            nn.Linear(6 * feature1_dim, feature1_dim)
         )
 
         self.layer2 = nn.Sequential(
