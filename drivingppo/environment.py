08083657e285ca986d541864d1996c6a6329deff
Evergood <arinmandri@gmail.com>
2026-02-05 01:02:13 +0900

    danger 로그 제거

--- drivingppo/environment.py
+++ drivingppo/environment.py
@@ -295,16 +295,12 @@ class WorldEnv(gym.Env):
             stat_progress     = + (cos_nx * s_norm) * 0.3  if s_norm > 0 \
                            else - s_norm * s_norm * 1.5  # 후진 진행 억제
             stat_orientation  = + cos_nx * 0.06
-            danger            = 0.0
-            danger_d          = 0.0
-            total = reward_time+stat_progress+stat_orientation+danger+danger_d
-            if self.render_mode == 'debug': print(f'REWARD: time {reward_time:+5.2f} |  prog {stat_progress:+5.2f} | ang {stat_orientation:+5.2f} | danger {danger:+5.2f} ~  {danger_d:+5.2f} --> {total:+6.2f}')
+            total = reward_time + stat_progress + stat_orientation
+            if self.render_mode == 'debug': print(f'REWARD: time {reward_time:+5.2f} |  prog {stat_progress:+5.2f} | ang {stat_orientation:+5.2f} --> {total:+6.2f}')
 
             reward_step[2] += self.step_per_control * reward_time
             reward_step[3] += self.step_per_control * stat_progress
             reward_step[4] += self.step_per_control * stat_orientation
-            reward_step[5] += self.step_per_control * danger
-            reward_step[6] += self.step_per_control * danger_d
 
         info = {'current_time': w.t_acc / 1000.0}
 
