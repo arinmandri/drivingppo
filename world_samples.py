ae2813486e1577204f9e03931cfb9540ee2b58ff
Evergood <arinmandri@gmail.com>
2026-02-15 14:33:18 +0900

    gen_2l num=10 * 각도 많이 꺾게 바꾸니까 복잡다.

--- world_samples.py
+++ world_samples.py
@@ -43,7 +43,7 @@ CAR_NEAR = math.sqrt(Car.w**2 + Car.h**2) / 2  # 장애물 피하기 기능을 �
 def gen_0():  return generate_random_world_plain(map_h= 50, map_w= 50, num=1,                min_dist=6,  max_dist=10, ang_init='half', ang_lim=0,      spd_init=0)
 def gen_1():  return generate_random_world_plain(map_h=150, map_w=150, num=3,                min_dist=6,  max_dist=20, ang_init='rand', ang_lim=pi*1.0, spd_init='rand')
 def gen_2():  return generate_random_world_plain(map_h=150, map_w=150, num=LOOKAHEAD_POINTS, min_dist=6,  max_dist=30, ang_init='rand', ang_lim=pi*1.0, spd_init='rand')
-def gen_2l(): return generate_random_world_plain(map_h=150, map_w=150, num=20,               min_dist=6,  max_dist=30, ang_init='rand', ang_lim=pi*1.0, spd_init='rand')
+def gen_2l(): return generate_random_world_plain(map_h=150, map_w=150, num=10,               min_dist=6,  max_dist=30, ang_init='rand', ang_lim=pi*1.0, spd_init='rand')
 def gen_obs():
     if randint(0, 1):
         choice = randint(0, 5)
