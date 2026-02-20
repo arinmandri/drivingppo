681e3d6509bf13c3136292eec1ede9b223e528d8
Evergood <arinmandri@gmail.com>
2026-02-20 12:33:03 +0900

    PP 튜닝 - 결과 중 모든 에피소드  결과 저장하지 않고 파라미터 조합별 성적 1 row씩만 저장 - 더 많은 경우 탐색
    연관 - 맵이 바뀌었으니 맞춰 재튜닝: dbb83f357c70659e922bbf5d8d81c0e7b5cf2c54

--- drivingppo/common.py
+++ drivingppo/common.py
@@ -35,7 +35,7 @@ OBSERVATION_DIM_LIDAR      =                                                    
 OBSERVATION_DIM            = 0 + 1 + (EACH_POINT_INFO_SIZE * LOOKAHEAD_POINTS) + LIDAR_NUM
 
 
-def set_seed(seed):
+def set_seed(seed, verbose=True):
     """
     seed 설정. 맵은 고정되는 듯.
     """
@@ -46,6 +46,6 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    print("SEED SET", seed)
+    if verbose: print("SEED SET", seed)
 
 # set_seed(0)
